6b60bf128d9d6fc3d1a2d8d2f0fcd877236711ea
NeverBehave <gayhub@never.pet>
2022-07-17 18:03:59 -0700

    typo

--- webhook.js
+++ webhook.js
@@ -9,4 +9,4 @@ const webhook_path = process.env.WEBHOOK_PATH ? process.env.WEBHOOK_PATH : "/sec
 const webhook_port = process.env.WEBHOOK_PORT ? process.env.WEBHOOK_PORT : 5000;
 
 console.log(`HTTP webhook started at ${webhook_path} and port ${webhook_port}`)
-bot.startWebhook('webhook_path', null, webhook_port)
+bot.startWebhook(webhook_path, null, webhook_port)
